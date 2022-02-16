0bcced4190eff76e52f82fbb69a9762bd440204f
jqwoutt <i.golovachev@tedu.app>
2022-02-16 14:07:39 +0300

    remove redurant

--- src/main/kotlin/app.tedu.api_generator/port/gradle/task/jar/GenerateApi.kt
+++ src/main/kotlin/app.tedu.api_generator/port/gradle/task/jar/GenerateApi.kt
@@ -1,8 +1,7 @@
 package app.tedu.api_generator.port.gradle.task.jar
 
 import app.tedu.api_generator.port.gradle.task.base.GenerateApi
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.*
+
 import java.io.File
 
 fun main() {
@@ -11,24 +10,4 @@ fun main() {
     val out = File(".", "out")
 
     GenerateApi(configPath = cfg.canonicalPath)(outputFile = out.canonicalPath)
-}
-
-
-fun test() {
-    val state = MutableSharedFlow<String>(extraBufferCapacity = 1)
-
-    CoroutineScope(Dispatchers.IO).launch {
-        state.collect {
-            println(it)
-        }
-    }
-
-    CoroutineScope(Dispatchers.IO).launch {
-        println(state.tryEmit("2"))
-        state.emit("1")
-        state.emit("3")
-        state.emit("4")
-    }
-
-    Thread.sleep(5000)
 }
